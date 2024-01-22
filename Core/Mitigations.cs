027316896bb266e14d5818acb33007c10717343e
Stargazing Koishi <i@sgkoi.dev>
2024-01-22 22:04:12 +0800

    Fix SwapWhileUse flagging

--- Core/Mitigations.cs
+++ Core/Mitigations.cs
@@ -62,7 +62,9 @@ public partial class Plugin
                 if (mitigation.SwapWhileUsePE)
                 {
                     var slot = BitConverter.ToInt16(data.Slice(1, 2));
-                    if (Terraria.Main.player[index].controlUseItem && slot == Terraria.Main.player[index].selectedItem)
+                    var type = BitConverter.ToInt16(data.Slice(6, 2))
+                   var existingItem = Terraria.Main.player[index].GetInventory(slot);
+                    if (Terraria.Main.player[index].controlUseItem && slot == Terraria.Main.player[index].selectedItem && type != existingItem.netID)
                     {
                         this.Statistics.MitigationRejectedSwapWhileUse++;
                         this.Detections.SwapWhileUseDetected(index, slot);
